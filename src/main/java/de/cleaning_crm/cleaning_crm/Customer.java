06cecddcc82da3383b825d4319a616f090e0fc2d
laila-terrak <lailaterrak@gmail.com>
2025-01-07 19:11:23 +0100

    create customer Entity

---
+++ src/main/java/de/cleaning_crm/cleaning_crm/Customer.java
@@ -0,0 +1,74 @@
+package de.cleaning_crm.cleaning_crm;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Customers")
+public class Customer {
+    @Id
+    @GeneratedValue
+    @Column(name = "ID")
+    private long id;
+
+    @Column(name = "First Name")
+    private String firstName;
+
+    @Column(name = "Last Name")
+    private String lastName;
+
+    @Basic(optional = false)
+    private String email;
+
+    //default JPA constructor
+    protected Customer() {
+    }
+
+    public Customer(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+}
