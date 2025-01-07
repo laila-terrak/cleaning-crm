e385d5f93f09a6a62d77f5132c749ede4f7480d3
laila-terrak <lailaterrak@gmail.com>
2025-01-07 15:16:18 +0100

    set up spring boot project

---
+++ src/test/java/de/cleaning_crm/cleaning_crm/ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.cleaning_crm.cleaning_crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
